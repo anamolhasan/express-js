81a0bd6e3f4bbb7f67f7407f39e2fbaedf8b38a3
anamolhasan <mdanamolhasan1998@gmail.com>
2025-10-29 10:06:25 +0600

    route and query paremater learn

--- express-basics/index.js
+++ express-basics/index.js
@@ -3,11 +3,28 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
-
+// get method
 app.get('/', (req, res) => {
-    console.log('Hello world')
+   res.send('Hello world')
+})
+// post method
+app.post('/post-data', (req, res) => {
+   res.send('Hello world')
 })
 
+// update method
+app.put('/data-update/:id', (req, res) => {
+   res.send('Hello world')
+})
+app.patch('/update/:id', (req, res) => {
+   res.send('Hello world')
+})
+// deleted method
+app.delete('/comments/:id', (req, res) => {
+   res.send('Hello world')
+})
+
+// request object
 app.get('/example', (req, res) => {
   console.log(req.params)
   console.log(req.query)
@@ -24,6 +41,19 @@ app.get('/example', (req, res) => {
   res.send('Hello World!-')
 })
 
+// route parameters
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id
+
+    res.send(`user Name: ${userId}`)
+})
+
+// query string parameters
+app.get('/search', (req, res) => {
+    const {category} = req.query
+    console.log(category)
+    res.send(`Search for ${category} on product page`)
+})
 
 app.listen(port, () => {
   console.log(`Server is listening on port http://localhost:${port}`)
