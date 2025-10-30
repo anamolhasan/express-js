eb9d7b686394b16240aa78593f3a0865ee93054d
anamolhasan <mdanamolhasan1998@gmail.com>
2025-10-30 10:09:38 +0600

    middleware learn

--- blog-api/index.js
+++ blog-api/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = require('./src/routes/blog_routes')
+const activityLogger = require('./src/middleware/logger')
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -7,7 +8,8 @@ const port = process.env.PORT || 3000
 // default middleware
 app.use(express.json())
 
-
+// use the custom middleware globally
+app.use(activityLogger)
 
 // using blogs routes
 const blogsRoute = router
