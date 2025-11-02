bd86f6c57b9ccd3f434e066586158777f9097a00
anamolhasan <mdanamolhasan1998@gmail.com>
2025-11-02 21:28:22 +0600

    user profile last part

--- user-profile-api/index.js
+++ user-profile-api/index.js
@@ -1,7 +1,12 @@
-
 const express = require('express')
 const app = express()
+const cookieParser = require('cookie-parser')
+const path = require('path')
+
 const port = process.env.PORT || 3000
+
+
+app.use(cookieParser())
 
 app.get('/api/profile', (req, res) => {
     const user = [
@@ -31,12 +36,38 @@ app.get('/api/profile', (req, res) => {
 
 // set cookies
 app.get('/set-custom-cookie', (req, res) => {
-    res.cookie('custom-cookie', '34532dfg866hff',{
+    res.cookie('customCookie', '34532dfg866hff',{
         maxAge:60000,
-        httpOnly:true
+        httpOnly:true,
+        secure:true
     })
 
     res.send(`Cookie set successfully `)
+})
+
+// get cookie
+app.get('/get-custom-cookie', (req, res) => {
+    const cookie = req.cookies.customCookie
+    console.log(cookie)
+
+    res.send(`Your custom cookie ${cookie}`)
+})
+// delete cookie
+app.get('/delete-custom-cookie', (req, res) => {
+    res.clearCookie('customCookie')
+    res.send(`Cookie deleted successfully!`)
+})
+
+
+// user profile
+app.get('/profile', (req, res) => {
+        const cookie = req.cookies.customCookie
+        if(!cookie){
+            return res.status(401).json({message:'Authorized access!'})
+        }
+    const filePath = path.join(process.cwd(), 'public', 'index.html')
+    console.log(filePath)
+    res.sendFile(filePath)
 })
 
 
