dd28a5663160339fea0c607915961cdad727e89d
anamolhasan <mdanamolhasan1998@gmail.com>
2025-11-04 01:40:00 +0600

    upload image multer

---
+++ upload-image-multer/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const multer  = require('multer')
+const path = require('path')
+const port = process.env.PORT || 3000
+
+
+const storage = multer.diskStorage({
+    destination: 'uploads/',
+    filename:(req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname))
+    } 
+})
+const upload = multer({storage})
+
+// middleware
+app.use(express.static('public'))
+
+
+app.post('/upload',upload.single('image'), (req, res) => {
+    if(!req.file) return res.status(400).json({error: "File not found"})
+
+        res.status(200).json({
+            message: "File uploaded successfully",
+            file: req.file.filename
+        })
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(port, () => {
+  console.log(`Server is listening on port http://localhost:${port}`)
+})
