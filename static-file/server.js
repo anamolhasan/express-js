1beec99d59d7fb1c4fb6221af137e42d66130a9c
anamolhasan <mdanamolhasan1998@gmail.com>
2025-11-04 00:44:49 +0600

    static file learn

---
+++ static-file/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = process.env.PORT || 3000
+
+// middleware
+app.use(express.static(path.join(process.cwd(), 'public')))
+
+app.get('/', (req, res) => {
+    const pathURL = path.join(process.cwd(), 'public', 'index.html')
+  res.sendFile('Hello World!')
+})
+
+
+app.listen(port, () => {
+  console.log(`Server is listening on port http://localhost:${port}`)
+})
